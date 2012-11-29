556fe1092ef8d25142a7063992133c37ec1c00a9
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2012-11-29 15:13:06 +0000

    Add a link to create new applications

--- spec/views/applications/index.html.erb_spec.rb
+++ spec/views/applications/index.html.erb_spec.rb
@@ -1,15 +1,21 @@
 require 'spec_helper'
 
 describe "applications/index.html.erb" do
-  it "displays all the applications" do
+  before do
     assign(:applications, [
       stub_model(Application, name: "slicer"),
       stub_model(Application, name: "dicer")
     ])
 
     render
+  end
 
+  it "displays all the applications" do
     rendered.should include("slicer")
     rendered.should include("dicer")
+  end
+
+  it "should have a link to create 'new' Applications" do
+    rendered.should have_link("Create Application", href: new_application_path)
   end
 end
