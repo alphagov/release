e687c4b3a8e544ae79e79e5da2f1b9e95f38d225
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2012-12-04 14:35:32 +0000

    Add capybara rails to the test helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,8 @@ require 'rspec/rails'
 require 'rspec/autorun'
 
 require 'capybara/rspec'
+require 'capybara/rails'
+
 require 'database_cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
