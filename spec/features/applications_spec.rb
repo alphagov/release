547728cf26a9359f880d846277ad6b89e8867cc3
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2012-12-04 14:57:30 +0000

    Add integration/feature tests for Applications
    This is the first pass at a set of integration tests for Applications.
    They are by no means complete but follow through the various bits of
    logic that we require to exist for:
    
     - creating
     - showing

---
+++ spec/features/applications_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe "Application management" do
+  describe "showing application information" do
+    let(:application_1) { Application.new(name: "beavis", repo: "beavis.com") }
+    let(:application_2) { Application.new(name: "butthead", repo: "butthead.com") }
+
+    before do
+      application_1.save
+      application_2.save
+
+      visit "/applications"
+    end
+
+    it "should have an OK response code" do
+      page.status_code.should eq(200)
+    end
+
+    it "should show the applications we have in our database" do
+      page.body.should include(application_1.name, application_2.name)
+    end
+
+    it "should have links to the applications" do
+      page.should have_link(application_1.name)
+      page.should have_link(application_2.name)
+    end
+  end
+
+  describe "creating applications" do
+    before do
+      visit "/applications"
+      click_on "Create Application"
+    end
+
+    it "should error when the form is submitted with empty fields" do
+      click_on "Create Application"
+      page.should have_content("There are some problems with the application")
+    end
+
+    describe "duplicating exisiting applications" do
+      let(:existing) { Application.new(name: "An existing app", repo: "thisexists.com") }
+
+      before do
+        existing.save
+      end
+
+      it "should error when fields are filled with details of an existing application" do
+        fill_in :application_name, with: existing.name
+        fill_in :application_repo, with: existing.repo
+
+        click_on "Create Application"
+
+        page.should have_content("There are some problems with the application")
+      end
+    end
+  end
+end
