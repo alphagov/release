1489ca87e13a34ee990a410e8fea77d5cafeea32
Jordan Hatch <jordan@jordanh.net>
2012-12-14 12:40:54 +0000

    Add DISPLAY=:99 to jenkins.sh to fix capybara-webkit

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 export RAILS_ENV=test
+export DISPLAY=":99"
+
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
 bundle exec rake db:drop db:create db:schema:load
 bundle exec rake ci:setup:rspec spec assets:clean assets:precompile
