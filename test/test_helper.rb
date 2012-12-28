81dd51f53ee6c8726db004e3258415d6e4bdc0ce
Kushal Pisavadia <kushi.p@gmail.com>
2012-12-28 16:00:31 +0000

    Add the minitest runner
    This is needed so that ci_reporter doesn't barf. Otherwise there's no
    runner for it to reference.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'shoulda-context'
+require 'minitest/autorun'
 require 'mocha/setup'
 
 require_relative '../spec/support/factories'
