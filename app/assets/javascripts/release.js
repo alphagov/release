a7411d02ac93591235ba83be6c46c7602a825132
Alex Muller <alex@mullr.net>
2013-10-13 14:39:30 +0100

    Sort environment columns based on deploy date
    Sort by datetime rather than alphabetically.

--- app/assets/javascripts/release.js
+++ app/assets/javascripts/release.js
@@ -1,5 +1,25 @@
 $(function () {
-  $('#application-list').tablesorter();
+  $.tablesorter.addParser({
+    id: 'release-num-and-date',
+    is: function (s) {
+      return false; // auto detection of this parser off
+    },
+    format: function (s, table, cell, cellIndex) {
+      return $(cell).attr('title');
+    },
+    type: 'text'
+  });
+
+  $('#application-list').tablesorter({
+    headers: {
+      2: {
+        sorter: 'release-num-and-date'
+      },
+      3: {
+        sorter: 'release-num-and-date'
+      }
+    }
+  });
 
   $("textarea#application_status_notes").autoGrow();
 
