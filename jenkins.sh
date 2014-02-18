7aec068559958e9b889cf65c7c28f2562e124a35
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-02-18 12:50:00 +0000

    Add a git clean to jenkins.sh
    This ensures we're running with a clean state.  It also prevents
    logfiles from gorwing without bounds on the CI machines

--- jenkins.sh
+++ jenkins.sh
@@ -2,6 +2,8 @@
 export RAILS_ENV=test
 export DISPLAY=":99"
 
+git clean -fdx
+
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
 bundle exec rake db:drop db:create db:schema:load
 bundle exec rake assets:clean assets:precompile
