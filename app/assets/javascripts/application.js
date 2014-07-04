7687d5cfff885c73231f3fc930b1dbe32d5ea814
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2014-07-04 07:22:28 +0000

    Switch to using govuk_admin_template layout.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,3 @@
-//= require jquery
-//= require jquery-ui
-//= require jquery_ujs
-//= require twitter/bootstrap
 //= require jquery_nested_form
 //= require jquery.tablesorter.min
 //= require jquery.autogrowtextarea
