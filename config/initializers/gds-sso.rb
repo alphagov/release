2b4c1f141f20e75a9289a04d5d6ac8e9fa0640ac
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-10-15 13:01:45 +0000

    Move all config secrets to env vars.
    Configuration for the database, the Rails secret token, GDS-SSO, and
    Github are now provided by env vars populated by Puppet.

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -1,6 +1,6 @@
 GDS::SSO.config do |config|
-  config.user_model   = 'User'
-  config.oauth_id     = 'release_oauth_id_defined_on_rollout'
-  config.oauth_secret = 'secret'
-  config.oauth_root_url = Plek.current.find("signon")
+  config.user_model = 'User'
+  config.oauth_id = ENV['OAUTH_ID']
+  config.oauth_secret = ENV['OAUTH_SECRET']
+  config.oauth_root_url = Plek.find("signon")
 end
