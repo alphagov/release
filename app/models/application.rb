72a4d45e8edb1b081a9108f54cc288d8e58bc10b
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-01-03 11:17:50 +0000

    Add empty line under guard clause

--- app/models/application.rb
+++ app/models/application.rb
@@ -19,6 +19,7 @@ class Application < ApplicationRecord
 
   def latest_deploy_to_each_environment
     return @latest_deploy_to_each_environment unless @latest_deploy_to_each_environment.nil?
+
     environments = deployments.select('DISTINCT environment').map(&:environment)
     @latest_deploy_to_each_environment = environments.each_with_object({}) do |environment, hash|
       hash[environment] = deployments.last_deploy_to(environment)
