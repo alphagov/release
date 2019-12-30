a9f631c127601dfe4b8cc3d820a6cb2761f9043a
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-12-30 16:49:41 +0000

    Co-locate URL helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,33 +30,6 @@ module ApplicationHelper
     end
   end
 
-  def github_tag_link_to(app, git_ref)
-    link_to(git_ref.truncate(15), "#{app.repo_url}/tree/#{git_ref}", target: "_blank", rel: "noopener", class: "govuk-link")
-  end
-
-  def github_compare_to_master(application, deploy)
-    "#{application.repo_url}/compare/#{deploy.version}...master"
-  end
-
-  def govuk_domain_suffix(environment, on_aws:)
-    return "blue.#{environment}.govuk.digital" if on_aws
-    return "publishing.service.gov.uk" if environment == "production"
-
-    "#{environment}.publishing.service.gov.uk"
-  end
-
-  def jenkins_deploy_app_url(application, release_tag, environment)
-    suffix = govuk_domain_suffix(environment, on_aws: application.on_aws?)
-    escaped_release_tag = CGI.escape(release_tag)
-    "https://deploy.#{suffix}/job/Deploy_App/parambuild?TARGET_APPLICATION=#{application.shortname}&TAG=#{escaped_release_tag}".html_safe # rubocop:disable Rails/OutputSafety
-  end
-
-  def jenkins_deploy_puppet_url(release_tag, environment, aws:)
-    suffix = govuk_domain_suffix(environment, on_aws: aws)
-    escaped_release_tag = CGI.escape(release_tag)
-    "https://deploy.#{suffix}/job/Deploy_Puppet/parambuild?TAG=#{escaped_release_tag}".html_safe # rubocop:disable Rails/OutputSafety
-  end
-
   def navigation_items
     return [] unless current_user
 
