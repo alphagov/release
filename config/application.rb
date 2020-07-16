29aedcd4e9d6c3c25f85568ef450ba1fea225260
Ben <benbaumann95@hotmail.co.uk>
2020-07-16 17:11:47 +0100

    Update configuration for Rails 6.0.3.2 - http://railsdiff.org/5.2.3/6.0.3.2
    Use config.load_defaults 6.0 and remove new framework defaults initializer
    
    Once the app is configured to load the latest defaults there is no
    need for the new defaults initializer as it only serves as a guide
    whilst upgrading the application.
    
    Remove auto-generated content_security_policy.rb file
    - govuk has their own way of managing security policy

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,13 @@
 require_relative "boot"
-
 require "rails"
 
 require "active_model/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_view/railtie"
-require "active_job/railtie"
-require "rails/test_unit/railtie"
 require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,11 +18,12 @@ require_relative "../lib/services"
 module ReleaseApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
