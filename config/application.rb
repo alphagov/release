adec9245a3d420099345d703860bfb8a285cc4a0
Ben <benbaumann95@hotmail.co.uk>
2020-09-02 12:15:16 +0100

    Fix asset pipeline with libsass
    * Fixes commit of accidently putting the config in environments/test.rb instead of application.rb
    
    Using a sass css compressor causes a scss file to be processed twice
    (once to build, once to compress) which breaks the usage of "unquote"
    to use CSS that has same function names as SCSS such as max.
    
    alphagov/govuk-frontend#1350

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,11 @@ module ReleaseApp
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = "London"
+
+    # Using a sass css compressor causes a scss file to be processed twice
+    # (once to build, once to compress) which breaks the usage of "unquote"
+    # to use CSS that has same function names as SCSS such as max.
+    # https://github.com/alphagov/govuk-frontend/issues/1350
+    config.assets.css_compressor = nil
   end
 end
