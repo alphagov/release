07c563990981d06ea77a61d0d0cc11c6d190532a
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2022-04-13 11:15:25 +0100

    Configure hotwire-livereload to refresh browser when asset updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,10 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.assets.precompile += %w( hotwire-livereload.js )
+  config.hotwire_livereload.force_reload_paths << Rails.root.join("app/assets/stylesheets")
+  config.hotwire_livereload.force_reload_paths << Rails.root.join("app/assets/javascript")
+
   config.hosts += [
     "release.dev.gov.uk",
   ]
