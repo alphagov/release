dfce81d3c9ac0529982c99a5789937873f2686be
Kevin Dew <kevindew@me.com>
2023-01-24 23:07:58 +0000

    Comment out GovukContentSecurityPolicy
    GOV.UK hadn't intended for this app to have the GOV.UK Content Security
    Policy yet, with us first planning to roll out this to frontend app. It
    looks like this was added as part of an outsourced Rails update [1],
    where the dev couldn't have known about our nuanced context.
    
    As this is an app that doesn't receive a lot of developer attention I'm
    disabling this as I don't want breaking changes to the CSP [2] to end up
    in this app.
    
    [1]: https://github.com/alphagov/release/pull/667
    [2]: https://github.com/alphagov/govuk_app_config/pull/279

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,1 +1,6 @@
-GovukContentSecurityPolicy.configure
+# Eventually we'll want to use the GOV.UK Content Security Policy in this app,
+# however as of January 2023 we're scoping it to only frontend apps
+#
+# For more info on the GOV.UK CSP see: https://docs.publishing.service.gov.uk/manual/content-security-policy.html
+#
+# GovukContentSecurityPolicy.configure
