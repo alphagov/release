b6c9c1178e7c808185fa7817e7c43a4879f27820
Sean Rankine <sean.rankine@me.com>
2023-03-23 12:28:35 +0000

    Fix environment references in tests
    The default is that EKS should be the live environments. So updating the
    deployment environment names in the test to match their EKS equivalent.

--- test/factories/factories.rb
+++ test/factories/factories.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
 
   factory :deployment do
     sequence(:version) { |n| "release_#{n}" }
-    environment { "production" }
+    environment { "production EKS" }
     application
   end
 
