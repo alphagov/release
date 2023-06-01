e79f7d922d9bc549c4f2f4c081f7816a9abd0c86
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2023-06-01 22:18:53 +0100

    Add a rake task to notify teams about apps that may need deploying
    The rake task will run once a day using Kubernetes cron job.

---
+++ test/unit/tasks/post_out_of_sync_deploys_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+require "rake"
+
+class PostOutOfSyncDeploysTest < ActiveSupport::TestCase
+  def setup
+    ReleaseApp::Application.load_tasks if Rake::Task.tasks.empty?
+  end
+
+  test "it should call PostOutOfSyncDeploysService" do
+    post_out_of_sync_deploys_service_mock = MiniTest::Mock.new
+    post_out_of_sync_deploys_service_mock.expect(:call, nil)
+
+    PostOutOfSyncDeploysService.stub(:call, post_out_of_sync_deploys_service_mock) do
+      Rake.application.invoke_task "post_out_of_sync_deploys"
+
+      assert_mock post_out_of_sync_deploys_service_mock
+    end
+  end
+end
