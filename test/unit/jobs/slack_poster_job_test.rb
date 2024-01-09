1904528b650373039373ebb1a0384b9512ef5958
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2024-01-09 12:48:55 +0000

    Remove unnecessary context block

--- test/unit/jobs/slack_poster_job_test.rb
+++ test/unit/jobs/slack_poster_job_test.rb
@@ -1,48 +1,46 @@
 require "test_helper"
 
 class SlackPosterJobTest < ActiveJob::TestCase
-  context "#perform" do
-    setup do
-      @webhook_url = "https://hooks.slack.com/services/T000/B00/XXX"
-    end
+  setup do
+    @webhook_url = "https://hooks.slack.com/services/T000/B00/XXX"
+  end
 
-    should "enqueue a job" do
-      assert_enqueued_with job: SlackPosterJob do
-        mock_env({ "SLACK_WEBHOOK_URL" => @webhook_url }) do
-          SlackPosterJob.perform_later("Hello", "#testchannel")
-        end
-      end
-    end
-
-    should "send a post request" do
+  should "enqueue a job" do
+    assert_enqueued_with job: SlackPosterJob do
       mock_env({ "SLACK_WEBHOOK_URL" => @webhook_url }) do
-        stub_post = stub_request(:post, @webhook_url).with(
-          body: "{\"username\":\"Release app\",\"icon_emoji\":\":govuk:\",\"mrkdwn\":\"true\",\"text\":\"Hello\",\"channel\":\"#testchannel\"}",
-        ).to_return(status: 200, body: "ok", headers: {})
-
-        SlackPosterJob.perform_now("Hello", "#testchannel")
-
-        assert_requested(stub_post)
+        SlackPosterJob.perform_later("Hello", "#testchannel")
       end
     end
+  end
 
-    should "send a post request when called with vaild options" do
-      mock_env({ "SLACK_WEBHOOK_URL" => @webhook_url }) do
-        stub_post = stub_request(:post, @webhook_url).with(
-          body: "{\"username\":\"Release app\",\"icon_emoji\":\":badger:\",\"mrkdwn\":\"true\",\"text\":\"Hello\",\"channel\":\"#testchannel\"}",
-        ).to_return(status: 200, body: "ok", headers: {})
+  should "send a post request" do
+    mock_env({ "SLACK_WEBHOOK_URL" => @webhook_url }) do
+      stub_post = stub_request(:post, @webhook_url).with(
+        body: "{\"username\":\"Release app\",\"icon_emoji\":\":govuk:\",\"mrkdwn\":\"true\",\"text\":\"Hello\",\"channel\":\"#testchannel\"}",
+      ).to_return(status: 200, body: "ok", headers: {})
 
-        SlackPosterJob.perform_now("Hello", "#testchannel", { "icon_emoji" => ":badger:" })
+      SlackPosterJob.perform_now("Hello", "#testchannel")
 
-        assert_requested(stub_post)
-      end
+      assert_requested(stub_post)
     end
+  end
 
-    should "not accept invalid options" do
-      perform_enqueued_jobs do
-        assert_raises(StandardError, 'Invalid options, only "username", "icon_emoji", "mrkdown" are permitted') do
-          SlackPosterJob.perform_later("Hello", "#testchannel", { "name" => "Slack poster", "icon_emoji" => ":badger:" })
-        end
+  should "send a post request when called with vaild options" do
+    mock_env({ "SLACK_WEBHOOK_URL" => @webhook_url }) do
+      stub_post = stub_request(:post, @webhook_url).with(
+        body: "{\"username\":\"Release app\",\"icon_emoji\":\":badger:\",\"mrkdwn\":\"true\",\"text\":\"Hello\",\"channel\":\"#testchannel\"}",
+      ).to_return(status: 200, body: "ok", headers: {})
+
+      SlackPosterJob.perform_now("Hello", "#testchannel", { "icon_emoji" => ":badger:" })
+
+      assert_requested(stub_post)
+    end
+  end
+
+  should "not accept invalid options" do
+    perform_enqueued_jobs do
+      assert_raises(StandardError, 'Invalid options, only "username", "icon_emoji", "mrkdown" are permitted') do
+        SlackPosterJob.perform_later("Hello", "#testchannel", { "name" => "Slack poster", "icon_emoji" => ":badger:" })
       end
     end
   end
