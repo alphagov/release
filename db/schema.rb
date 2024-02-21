1d487da0f5cdb6d7fe3a1ee4285e145fc6006453
Sean Rankine <sean.rankine@me.com>
2024-02-21 15:55:37 +0000

    Add migration for removing EKS suffix from environment names
    This normalises the environment names, as we no longer need to
    differentiate between EC2 and EKS deployed environments.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_08_111830) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_21_154421) do
   create_table "applications", id: :integer, charset: "latin1", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: nil, null: false
