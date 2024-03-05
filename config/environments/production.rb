a6cc313acf4c30856762b38459b8f0fa196a84af
D Chua <deborah.chua@digital.cabinet-office.gov.uk>
2024-03-05 11:46:12 +0000

    Delete unused Sass configuration
    This was previously added for `sassc-rails` and is no longer needed.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,10 +28,6 @@ Rails.application.configure do
 
   config.assets.js_compressor = :uglifier
 
-  # Rather than use a CSS compressor, use the SASS style to perform compression.
-  config.sass.style = :compressed
-  config.sass.line_comments = false
-
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
