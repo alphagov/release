f774cd6ff0bf038606b177b72a94300b78654abd
Murilo Dal Ri <murilodalri@gmail.com>
2024-05-13 15:30:30 +0100

    Remove unused methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,4 @@
 module ApplicationHelper
-  def nav_link(text, link)
-    recognized = Rails.application.routes.recognize_path(link)
-    if recognized[:controller] == params[:controller] && recognized[:action] == params[:action]
-      tag.li(class: "active") do
-        link_to(text, link, class: "govuk-link")
-      end
-    else
-      tag.li do
-        link_to(text, link, class: "govuk-link")
-      end
-    end
-  end
-
   def human_datetime(date)
     if date
       if date.today?
