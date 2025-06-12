ea3342586ca949937ad49b30958b73246c2041c7
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2025-06-12 11:33:34 +0100

    wip refactor Services for k8s service

--- app/lib/services.rb
+++ app/lib/services.rb
@@ -16,6 +16,7 @@ module Services
     AWS_REGION = "eu-west-1".freeze # TODO probably not needed
     # https://github.com/alphagov/govuk-infrastructure/blob/main/terraform/deployments/release/assumed.tf
     ASSUMED_ROLE_ARN = {
+      "development" => "arn:aws:iam::210287912431:role/release-assumed",
       "integration" => "arn:aws:iam::210287912431:role/release-assumed",
       "staging" => "arn:aws:iam::696911096973:role/release-assumed",
       "production" => "arn:aws:iam::172025368201:role/release-assumed",
@@ -38,7 +39,7 @@ module Services
       "k8s-aws-v1.#{Base64.strict_encode64(url).gsub('=', '')}"
     end
 
-    def kube_client(environment: "integration")
+    def k8s(environment: "integration", version: "v1")
       credentials = Aws::AssumeRoleCredentials.new({ 
         client: Aws::STS::Client.new(region: AWS_REGION), 
         role_arn: ASSUMED_ROLE_ARN[environment], 
@@ -63,21 +64,53 @@ module Services
 
       Kubeclient::Client.new(
         cluster.endpoint, 
-        'v1',
+        version,
         auth_options: auth_options,
         ssl_options: ssl_options 
       )
     end
 
-    def get_pods(repo_name:, environment: "integration")
-      client = kube_client(environment: environment)
-      client.get_pods(namespace: 'apps', labelSelector: "app.kubernetes.io/name=#{repo_name}")
+    def get_pods_by_status(environment:, repo_name:, status:)
+      client = k8s(environment: environment)
+      client.get_pods(namespace: 'apps', label_selector: "app.kubernetes.io/name=#{repo_name}", field_selector: { "status.phase": status })
+    end
+
+    def get_running_pods(repo_name:, environment: "integration")
+      res = []
+      pods = get_pods_by_status(environment: environment, repo_name: repo_name, status: "Running")
+      pods.each do |pod|
+        images = []
+        pod["spec"]["containers"].each { |c| images.append({ "image" => c["image"] }) }
+        creation_timestamp = pod["metadata"]["creationTimestamp"]
+        res.append({
+          "name" => pod["metadata"]["name"],
+          "images" => images,
+          "createdAt" => creation_timestamp,
+        })
+      end
+      res
+    end
+
+    def get_k8s_image_tag(environment, repo_name)
+      pods = get_running_pods(repo_name: repo_name, environment: environment)
+      if pods != []
+        {
+          "image" => pods[0]["images"][0]["image"].split(":")[-1],
+          "created_at" => pods[0]["createdAt"],
+        }
+      else
+        {
+          "image" => "None",
+          "created_at" => "",
+        }
+      end
     end
   end
 end
 
-puts "======integration======"
-puts Services.get_pods(repo_name: "asset-manager", environment: "integration")
+# puts "======integration======"
+# puts Services.get_running_pods(environment: "integration", repo_name: "signon")
+# puts Services.get_k8s_image_tag("integration", "signon")
 
-puts "======staging======"
-puts Services.get_pods(repo_name: "asset-manager", environment: "staging")
+# # puts "======staging======"
+# # puts Services.get_image_tag(repo_name: "asset-manager", environment: "staging")
