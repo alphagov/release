d11d8fb19136fb318c407295b7eb9fa9d5c3e5c7
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2025-06-23 19:57:45 +0100

    Update human_datetime test description

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,11 +2,11 @@ require "test_helper"
 
 class ApplicationHelperTest < ActionView::TestCase
   context "hunan_datetime" do
-    should "can handle date strings" do
+    should "handle date strings" do
       assert_equal "2:27pm on 29 Jan", human_datetime("2025-01-29T14:27:01Z")
     end
 
-    should "can handle invalid date strings" do
+    should "handle invalid date strings" do
       assert_equal "", human_datetime("error date")
     end
   end
