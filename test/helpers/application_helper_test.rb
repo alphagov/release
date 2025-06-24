6dbf302aa965db764bf6bce67311147b4ca91e99
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2025-06-24 14:18:52 +0100

    Update appliication_helper_test
    Also check that it can handle DateTime objects

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,6 +2,10 @@ require "test_helper"
 
 class ApplicationHelperTest < ActionView::TestCase
   context "hunan_datetime" do
+    should "handle date objects" do
+      assert_equal "2:27pm on 29 Jan", human_datetime(Time.zone.parse("2025-01-29T14:27:01Z"))
+    end
+
     should "handle date strings" do
       assert_equal "2:27pm on 29 Jan", human_datetime("2025-01-29T14:27:01Z")
     end
