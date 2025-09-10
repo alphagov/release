b52100506df037fbc3c2bfcfd71e61d28f024499
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2025-09-10 12:47:05 +0100

    Rewrite Model (unit) tests using RSpec
    As per Rails convention, the directories within the spec directory contain Ruby
    unit tests with the directories matching those within the app directory
    (models).
    
    Key changes:
    - removed explicit `name: SecureRandom.hex` on FactoryBot.create(:application)
    - removed unnecessary Deployment.delete_all
    - removed unnecessary context nesting (e.g. given valid attributes etc.)
    - used let/before for DRY setup
    - grouped tests logically using describe blocks for methods
    - used the Ruby documentation convention of `.` when referring to a class
      method’s name and `#` when referring to an instance method’s name
    - replaced FactoryBot :create with :build when appropriate
    - removed duplicated "current image deployed by environment" test for
      Application
    
    Applications "display datetimes" tests will be moved from application to
    application_helper.

--- spec/models/repo_spec.rb
+++ spec/models/repo_spec.rb
@@ -1,48 +1,49 @@
-require "test_helper"
-
-class RepoTest < ActiveSupport::TestCase
+RSpec.describe Repo do
   describe ".all" do
-    should "return an array of repositories" do
+    it "returns an array of repositories" do
       stub_request(:get, Repo::REPO_JSON_URL).to_return(
         status: 200,
         body: '[{"name": "repo1"}, {"name": "repo2"}]',
       )
-      repos = Repo.all
+      repos = described_class.all
 
-      assert_instance_of Array, repos
-      assert_equal 2, repos.length
+      expect(repos).to be_an(Array)
+      expect(repos.length).to eq(2)
     end
 
-    should "handle HTTP errors gracefully" do
+    it "handles HTTP errors gracefully" do
       stub_request(:get, Repo::REPO_JSON_URL).to_return(status: 404)
 
-      repos = Repo.all
-      assert_instance_of Array, repos
-      assert_empty repos
+      repos = described_class.all
+
+      expect(repos).to be_an(Array)
+      expect(repos).to be_empty
     end
   end
 
   describe ".url" do
-    should "get the GitHub URL for a repository" do
+    it "gets the GitHub URL for a repository" do
       response_body = [{
         "app_name" => "account-api",
         "links" => { "repo_url" => "https://github.com/alphagov/account-api" },
       }].to_json
+
       stub_request(:get, Repo::REPO_JSON_URL).to_return(status: 200, body: response_body)
 
-      assert_equal "https://github.com/alphagov/account-api", Repo.url(app_name: "Account API")
+      expect(described_class.url(app_name: "Account API")).to eq("https://github.com/alphagov/account-api")
     end
   end
 
   describe ".shortname" do
-    should "get the shortname for a repository" do
+    it "gets the shortname for a repository" do
       response_body = [{
         "app_name" => "account-api",
         "shortname" => "account_api",
       }].to_json
+
       stub_request(:get, Repo::REPO_JSON_URL).to_return(status: 200, body: response_body)
 
-      assert_equal "account_api", Repo.shortname(app_name: "account-api")
+      expect(described_class.shortname(app_name: "account-api")).to eq("account_api")
     end
   end
 end
