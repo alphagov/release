755269c807d4515e6fe5e6854c66948e0fa420ba
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2025-09-10 12:31:34 +0100

    Configure DatabaseCleaner for reliable test isolation
    Add Database Cleaner setup to ensure a clean database state between specs.
    Uses truncation before the suite and transactions for each example to prevent
    issues like ActiveRecord::RecordInvalid from leftover singleton records (e.g.
    Site).
    
    This improves test reliability and prevents validation errors caused by
    persisted data between tests.
    
    It was also used in Minitest

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,19 @@ GovukTest.configure
 WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
+
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true # will default to `true` in RSpec 4.
   end
