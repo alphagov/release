80738200966c42ae5f1539bcfb8c5f383dfc6b18
kashifatcha <kashif.atcha@digital.cabinet-office.gov.uk>
2025-12-03 09:59:33 +0000

    Add comment 'rubocop:disable Rails/BulkChangeTable' to avoid linting error

--- db/migrate/20251118085631_add_enable_change_failure_marking_to_applications.rb
+++ db/migrate/20251118085631_add_enable_change_failure_marking_to_applications.rb
@@ -1,6 +1,6 @@
 class AddEnableChangeFailureMarkingToApplications < ActiveRecord::Migration[7.2]
   def change
-    add_column :applications, :enable_change_failure_marking, :boolean, default: false, null: false
+    add_column :applications, :enable_change_failure_marking, :boolean, default: false, null: false # rubocop:disable Rails/BulkChangeTable
     add_column :applications, :slack_channel_deployment_notification, :string
   end
 end
